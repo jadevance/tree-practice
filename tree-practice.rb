889ffd8805535f66d464f0ae15cedaee83215722
Jade Vance <jadenicolevance@gmail.com>
2016-06-24 15:27:21 -0700

    Added in tree nodes for the in-class examples

--- tree-practice.rb
+++ tree-practice.rb
@@ -8,6 +8,7 @@ class TreeNode
    end
 end
 
+# recusrive 
 def print_infix(node)
   return if node == nil
   print_infix(node.left)
@@ -15,7 +16,43 @@ def print_infix(node)
   print_infix(node.right)
 end
 
+def operators(node)
+  return if node == nil
+  operators(node.left)
+  if node.left != nil || node.right != nil
+    print node.value + " "
+  end 
+  operators(node.right)
+end
+
+
+
 root = TreeNode.new("+")
 root.left = TreeNode.new("3")
 root.right = TreeNode.new("2")
-print_infix(root)
+puts print_infix(root)
+
+root = TreeNode.new("-")
+root.left = TreeNode.new("+")
+root.right = TreeNode.new("10")
+root.left.left = TreeNode.new("3")
+root.left.right = TreeNode.new("2")
+puts print_infix(root)
+
+root = TreeNode.new("+")
+root.left = TreeNode.new("*")
+root.right = TreeNode.new("2")
+root.left.left = TreeNode.new("4")
+root.left.right = TreeNode.new("3")
+puts print_infix(root)
+
+root = TreeNode.new("-")
+root.left = TreeNode.new("+")
+root.right = TreeNode.new("%")
+root.right.left = TreeNode.new("10")
+root.right.right = TreeNode.new("5")
+root.left.left = TreeNode.new("*")
+root.left.right = TreeNode.new("2")
+root.left.left.left = TreeNode.new("4")
+root.left.left.right = TreeNode.new("3")
+puts print_infix(root)
